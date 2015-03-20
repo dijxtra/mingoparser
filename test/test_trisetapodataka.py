ddfaa9d793c45c7c56c3354e15e24acca3a59cbd
Nikola Škorić <nskoric@gmail.com>
2015-03-20 06:04:55 +0100

    Dodan jos jedan test.

---
+++ test/test_trisetapodataka.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+import unittest
+from mingoparser import *
+
+
+class DvaSetaPodataka(unittest.TestCase):
+    """Testiranje aplikacije sa dva različita seta podataka odjednom."""
+
+    def setUp(self):
+        self.baza = DatabaseConnection('db.test.sqlite3')
+        self.baza.kreiraj_tablice()
+
+        citac = CitacVrijednostiOffline('inputs2/')
+        vlasnici_citani = citac.gen_vlasnici_full()
+        self.baza.popuni_osnovne_tablice(vlasnici_citani, '2015-03-10 10:03:13')
+        self.baza.popuni_tablice(vlasnici_citani, '2015-03-10 10:03:13')
+
+        citac = CitacVrijednostiOffline('inputs/')
+        vlasnici_citani = citac.gen_vlasnici_full()
+        self.baza.popuni_osnovne_tablice(vlasnici_citani, '2015-03-12 7:03:43')
+        self.baza.popuni_tablice(vlasnici_citani, '2015-03-12 7:03:43')        
+
+        citac = CitacVrijednostiOffline('inputs2/')
+        vlasnici_citani = citac.gen_vlasnici_full()
+        self.baza.popuni_osnovne_tablice(vlasnici_citani, '2015-03-14 08:26:34')
+        self.baza.popuni_tablice(vlasnici_citani, '2015-03-14 08:26:34')        
+
+        citac = CitacVrijednostiOffline('inputs2/')
+        vlasnici_citani = citac.gen_vlasnici_full()
+        self.baza.popuni_osnovne_tablice(vlasnici_citani, '2015-03-17 17:53:41')
+        self.baza.popuni_tablice(vlasnici_citani, '2015-03-17 17:53:41')        
+        
+        self.vlasnici = self.baza.citaj_vlasnike()
+        self.vlasnici = self.baza.citaj_indekse(self.vlasnici)
+        self.vlasnici = self.baza.citaj_cijene_s_postajama(self.vlasnici)
+
+
+    def test_vrijeme_zadnjeg_upisa(self):
+        self.assertEqual(self.baza.vrijeme_zadnjeg_upisa(), u'2015-03-17 17:53:41')
+        
+        self.assertEqual(self.baza.vrijeme_zadnjeg_upisa(vrsta_goriva = 2), u'2015-03-17 17:53:41')
+        self.assertEqual(self.baza.vrijeme_zadnjeg_upisa(vrsta_goriva = 3), u'2015-03-17 17:53:41')
+        
+    def test_jedan_indeks(self):
+        vrsta_goriva = 2
+        limit = 4
+        
+        vlasnik = self.vlasnici[88]
+
+        self.assertEqual(vlasnik.ime(), 'Konzum d.d.')
+        self.assertEqual(vlasnik.cijene_sa_brojem_postaja(vrsta_goriva), {9.59: 4, 9.22: 4})
+        self.assertEqual(vlasnik.broj_postaja(vrsta_goriva), 8)
+        self.assertEqual(round(vlasnik.indeks(vrsta_goriva), 2), 9.59)
+        self.assertEqual(vlasnik.vrijeme_zadnjeg_upisa(vrsta_goriva), u'2015-03-17 17:53:41')
+
+if __name__ == '__main__':
+    unittest.main()
